bae845d1fb0455ad299ac0f04d1db48956033c26
Adam Guarino <33468165+adamguarino@users.noreply.github.com>
2018-08-08 17:06:17 -0400

    *WIP* Code for connecting to Watson Translator API

--- watsonapi.py
+++ watsonapi.py
@@ -1,11 +1,15 @@
 import json
 from watson_developer_cloud import LanguageTranslatorV3
 
+#Establishes the version and key used to connect to the translator. Key should be replaced with the key generated for you on IBM's website
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     iam_api_key='iam_api_key'
 )
+
+#Allows the user to input text in the console to be translated
+inputText = input("Enter text: ")
 translation = language_translator.translate(
-    text='Hello',
+    text=inputText,
     model_id='en-es')
 print(json.dumps(translation, indent=2, ensure_ascii=False))
