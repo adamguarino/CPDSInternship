cc2b32fbdb8f3f96ed26849d5d85dd4bf7fe0401
Paul Maino <pcmaino@gmail.com>
2018-08-08 12:53:27 -0400

    added docstrings

--- xlsx.py
+++ xlsx.py
@@ -1,8 +1,10 @@
 import xlsxwriter
 
+#create a new Excel file
 workbook = xlsxwriter.Workbook('hello.xlsx')
 worksheet = workbook.add_worksheet()
 
+#writes to a specific cell
 worksheet.write('A1', 'Hello world')
 
 workbook.close()
