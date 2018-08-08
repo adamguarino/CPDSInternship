e2e302a85af0736bd3f22f0fd81b9f2a9bbbc5cd
Adam Guarino <33468165+adamguarino@users.noreply.github.com>
2018-08-08 16:53:25 -0400

    Calls the IBM i API and returns the data as JSON

--- ibmapi.py
+++ ibmapi.py
@@ -2,9 +2,11 @@ import requests
 from databaseGUI import *
 import json
 
+#Initializes the main loop for the GUI which takes text input
 gui = Gui()
 classNum = gui.startLoop()
 
+#Calls the IBM i API with whichever number was input, adds a JSON header, and returns the data as JSON
 def get_data():
     api_url = "http://192.168.200.115:10208//web/services/RTVITEMSVCCGREST/itemclass/" + classNum
     request_data = requests.get(api_url, headers={"Accept":"application/json"})
@@ -14,5 +16,6 @@ def get_data():
 data = get_data()
 print(data.json())
 
+#Creates a writable json file and writes the returned JSON data to the file
 with open("data_file.json", "w") as write_file:
     json.dump(data.json(), write_file)
