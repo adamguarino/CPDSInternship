fb5a3daa92fce72a229d2ed6ccf676104183d525
Adam Guarino <33468165+adamguarino@users.noreply.github.com>
2018-08-08 13:10:47 -0400

    Translates to and from a chosen language

--- translate.py
+++ translate.py
@@ -1,11 +1,15 @@
 import boto3
+#Prompts you to enter a two letter code in the console for the language you want to translate to
+newLanguage = input('Enter language: ') 
 client = boto3.client('translate')
 response = client.translate_text(
+    #Prompts you to enter text in the console that you want to translate
     Text = input('Enter text: '),
     SourceLanguageCode = 'en',
-    TargetLanguageCode = 'it'
+    TargetLanguageCode = newLanguage,
     )
 print(response)
+#Translates from target language back to English
 reverse = client.translate_text(
     Text = response['TranslatedText'],
     SourceLanguageCode = response['TargetLanguageCode'],
