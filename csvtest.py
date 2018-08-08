3d59e686903715483ea90f6aab0f311c14caab7b
Adam Guarino <33468165+adamguarino@users.noreply.github.com>
2018-08-08 16:09:39 -0400

    Writes to a CSV file with hard-coded data

--- csvtest.py
+++ csvtest.py
@@ -1,11 +1,14 @@
 import csv
 def main():
+    #Creates an array with hard-coded data to be stored
     myData = [["first_name", "second_name", "Grade"],
               ['Alex', 'Brian', 'A'],
               ['Tom', 'Smith', 'B']]
     print("Declaring file object")
+    #Creates a file called example.csv, "w" means you can write to the file  
     myFile = open('example.csv', 'w')
     print("Writing data")
+    #Writes the above data to the file
     with myFile:
         writer = csv.writer(myFile)
         writer.writerows(myData)
