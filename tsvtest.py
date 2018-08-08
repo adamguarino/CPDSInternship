6c375e4d40c0145a25ddcccb22e56f644974c95c
Adam Guarino <33468165+adamguarino@users.noreply.github.com>
2018-08-08 15:28:54 -0400

    Writes to a TSV file with hard-coded data

--- tsvtest.py
+++ tsvtest.py
@@ -1,7 +1,9 @@
 import tsv
 def main():
+    #Creates a file called example.tsv, "w" means you can write to the file
     myFile = open('example.tsv', 'w')
     with myFile:
+        #Writes to the file and includes whatever data you put in here
         writer = tsv.TsvWriter(myFile)
         writer.line("first_name", "second_name", "Grade")
         writer.line('Alex', 'Brian', 'A')
