c575ef46df5c2dbaeb7f3c4a2a5d862883ac443a
Paul Maino <pcmaino@gmail.com>
2018-08-08 11:44:16 -0400

    added docstrings

--- comprehend.py
+++ comprehend.py
@@ -1,20 +1,22 @@
+#https://boto3.readthedocs.io/en/latest/reference/services/comprehend.html
+
 import boto3
 client = boto3.client('comprehend')
 input = input('Enter text: ')
 response = []
-response.append(client.detect_sentiment(
+response.append(client.detect_sentiment(    #detects the "Tone" of the inputted text: Positive, Neutral, Mixed or Negative
     Text = input,
     LanguageCode='en'
     ))
-response.append(client.detect_entities(
+response.append(client.detect_entities(     #detects proper nouns, dates, etc. in the input
     Text = input,
     LanguageCode='en'
     ))
-response.append(client.detect_key_phrases(
+response.append(client.detect_key_phrases(  #detects key phrases and ideas in the input
     Text = input,
     LanguageCode='en'
     ))
-response.append(client.detect_syntax(
+response.append(client.detect_syntax(       #detects the part of speech for each word in the input
     Text = input,
     LanguageCode='en'
     ))
